be2d98a9a00afcaf88869b3abf21abd2e54bfa9a
osmktok <o_k.2o11@icloud.com>
2023-01-13 18:14:12 +0900

    3章 : メモ機能を作成しよう
    ディレクトリ構成について理解しよう

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
- 
+const morgan = require('morgan')
+
+app.use(express.json())
+
+app.use(morgan('common'))
+
 app.get('/', (req, res) => {
- res.json({
-   message: 'Hello World!'
- })
+  res.json({
+    message: 'Hello World!'
+  })
 })
- 
+
 app.listen(port, () => {
- console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
