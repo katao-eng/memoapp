667a14340448d4b543c86fe53e6f2a56cd86c7c1
osmktok <o_k.2o11@icloud.com>
2023-01-15 07:22:21 +0900

    3章 : メモ機能を作成しよう
    メモの閲覧機能の実装
    controller層の実装

---
+++ controller/memo.js
@@ -0,0 +1,4 @@
+exports.getAll = (req, res, next) => {
+  // TODO: usecase層からデータを取得できるようにする
+  return res.status(200).send({})
+}
--- index.js
+++ index.js
@@ -2,16 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 const morgan = require('morgan')
+const controller = require('./controller')
 
 app.use(express.json())
 
 app.use(morgan('common'))
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello World!'
-  })
-})
+app.use('/memo', controller.memo())
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
