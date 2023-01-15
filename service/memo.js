a896e949474249e420cd0f4c566da748dfdeac96
osmktok <o_k.2o11@icloud.com>
2023-01-16 04:32:08 +0900

    メモの追加機能の実装
    service層の作成

--- service/memo.js
+++ service/memo.js
@@ -1,9 +1,25 @@
 const model = require('../model')
+const mongoose = require('mongoose')
 
 exports.getAll = async() => {
   try {
     const memos = await model.memo.find({}) || []
     return memos
+  } catch (e) {
+    throw e
+  }
+}
+
+exports.create = async(message, checked) => {
+  try {
+    await model.memo.create(
+      {
+        _id: mongoose.Types.ObjectId(),
+        message,
+        checked,
+      }
+    )
+    return
   } catch (e) {
     throw e
   }
