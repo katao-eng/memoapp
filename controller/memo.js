78812ea77784a35e5cda924c915c1ff5a15ede6c
osmktok <o_k.2o11@icloud.com>
2023-01-17 04:21:47 +0900

    メモの編集機能の実装
    controller層の実装

--- controller/memo.js
+++ controller/memo.js
@@ -22,3 +22,14 @@ exports.create = async(req, res, next) => {
     return res.status(500).send({"error": String(e), "message": "error occured"})
   }
 }
+
+exports.update = async(req, res, next) => {
+  const id = req.params.id
+  const checked = req.body.checked
+  try {
+    await usecase.memo.update(id, checked)
+    return res.status(200).send({})
+  } catch (e) {
+    return res.status(500).send({"error": String(e), "message": "error occured"})
+  }
+}
